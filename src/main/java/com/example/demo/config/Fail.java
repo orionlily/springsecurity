606e8e907279044ea75bb00fd41cf72302a1f329
li.lc <li.lc@starcart.cn>
2020-05-09 18:25:36 +0800

    upload

--- src/main/java/com/example/demo/config/Fail.java
+++ src/main/java/com/example/demo/config/Fail.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import com.example.demo.util.JsonUtil;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
@@ -7,6 +8,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author li.lc
@@ -15,6 +18,11 @@ public class Fail implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         String errMsg = exception.getMessage();
-        response.getWriter().write(errMsg);
+        Map<String,Object> paramMap = new HashMap<>(2);
+        paramMap.put("success",Boolean.FALSE);
+        paramMap.put("msg",errMsg);
+        response.setContentType("Content-type:application/json");
+        String result = JsonUtil.ObjToJson(paramMap);
+        response.getWriter().write(result);
     }
 }
